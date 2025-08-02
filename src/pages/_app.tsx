b9c6c38f99bbb58f73ba8ebd205a26469a084b22
HealTea Developer <developer@healtea.com>
2025-08-02 16:51:26 +0900

    Google Analytics設定を改善: 環境変数チェックとデバッグ情報を追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,23 +2,31 @@ import type { AppProps } from 'next/app'
 import Script from 'next/script'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const GA_ID = process.env.NEXT_PUBLIC_GA_ID
+
   return (
     <>
       {/* Google tag (gtag.js) */}
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-        strategy="afterInteractive"
-      />
-      <Script id="gtag-init" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
-            page_path: window.location.pathname,
-          });
-        `}
-      </Script>
+      {GA_ID && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="gtag-init" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}', {
+                page_path: window.location.pathname,
+                debug_mode: ${process.env.NODE_ENV === 'development' ? 'true' : 'false'}
+              });
+              console.log('Google Analytics initialized with ID: ${GA_ID}');
+            `}
+          </Script>
+        </>
+      )}
 
       <Component {...pageProps} />
     </>
